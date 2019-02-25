fcf37cd381f0d3c04d5268b8ecedeeb73b1e956b
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-02-25 21:51:34 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import time
+import sys
+import tweepy
+import datetime
+
+#from local_settings import *  # use this one for testing
+
+# use this for production; set vars in heroku dashboard
+from os import environ
+Consumer_key = environ['Consumer_key']
+Consumer_secret = environ['Consumer_secret']
+Access_key = environ['Access_key']
+Access_secret = environ['Access_secret']
+
+#INTERVAL = 60 * 60 * 6  # tweet every 6 hours
+#INTERVAL = 60  # every 15 seconds, for testing
+
+auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
+auth.set_access_token(Access_key, Access_secret)
+api = tweepy.API(auth)
+
+if __name__ == '__main__':
+    print("about to update status...")
+    api.update_status('Right now it is ' + str(datetime.datetime.now()))
+ #   time.sleep(INTERVAL)

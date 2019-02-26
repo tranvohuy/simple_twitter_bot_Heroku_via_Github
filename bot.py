1cdad8a71544e0665f07801dffe490d5785c4424
tranvohuy <39524209+tranvohuy@users.noreply.github.com>
2019-02-26 10:28:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,12 @@ import sys
 import tweepy
 import datetime
 
-#from local_settings import *  # use this one for testing
-
-# use this for production; set vars in heroku dashboard
 from os import environ
 Consumer_key = environ['Consumer_key']
 Consumer_secret = environ['Consumer_secret']
 Access_key = environ['Access_key']
 Access_secret = environ['Access_secret']
 
-#INTERVAL = 60 * 60 * 6  # tweet every 6 hours
-#INTERVAL = 60  # every 15 seconds, for testing
 
 auth = tweepy.OAuthHandler(Consumer_key, Consumer_secret)
 auth.set_access_token(Access_key, Access_secret)
@@ -22,4 +17,3 @@ api = tweepy.API(auth)
 if __name__ == '__main__':
     print("about to update status...")
     api.update_status('This bot is deployed via  Heroku dashboard connecting to Github. Right now the time is ' + str(datetime.datetime.now()))
- #   time.sleep(INTERVAL)
